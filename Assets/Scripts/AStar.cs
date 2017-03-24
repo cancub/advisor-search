d34f3445845977eceab805591a7cca879671f7ca
cancub <alfred.kenny@mail.mcgill.ca>
2017-03-24 02:51:14 -0400

    untested A*
    code written but untested

--- Assets/Scripts/AStar.cs
+++ Assets/Scripts/AStar.cs
@@ -4,66 +4,183 @@ using UnityEngine;
 
 public struct gameTile {
 	public Vector2 position;
-	public int distance;
+	public int G,F,id,parent;
 }
 
 
 
 public class AStar {
-	public AStar(Vector2 start, Vector2 dest) {
-		A = dest;
-		B = start;
+	public AStar() {
 		directions.Add (Vector2.up);
 		directions.Add (Vector2.left);
 		directions.Add (Vector2.down);
 		directions.Add (Vector2.right);
 		gc = (GameController)GameObject.Find ("GameController").GetComponent (typeof(GameController));
+		maxID = 0;
 	}
 		
 	public List<Vector2> directions;
-	public Vector2 A;
-	public Vector2 B;
 	public GameController gc;
+	public int maxID; // ids are used to break ties in terms of F distance
 
-	public List<Vector2> navigate() {
-		
+	public List<Vector2> navigate(Vector2 start, Vector2 dest) {
+		gameTile currentTile;
+		gameTile adjacentTile;
+
+		// the initialization phase
 		List<gameTile> open = new List<gameTile> ();
 		List<gameTile> closed = new List<gameTile> ();
-		List<Vector2> finalPath = new List<Vector2> ();
 
 		// add current position to closed list
-		gameTile tile;
-		tile.position = A;
-		tile.distance = 0;
-		closed.Add(tile);
+		currentTile.position = start;
+		currentTile.G = 0;
+		currentTile.F = getH (currentTile.position, dest) + currentTile.G;
+		currentTile.id = maxID++;
+		currentTile.parent = currentTile.id;
+		closed.Add(currentTile);
 
 		// add all manhattan adjacent tiles to open list if they are not in an obstacle
-		foreach (Vector2 dir in directions) {
+		for( int i = 0; i < 4; i++) {
 
-			if (!gc.inObstacle (dir + A)) {
+			if (!gc.inObstacle (directions[i] + start)) {
 				// create a new gameTile for this location
-				tile.position = dir + A;
-//				tile.distance = 
-				open.Add (tile);
+				adjacentTile.position = directions[i] + start;
+				adjacentTile.G = 1;
+				adjacentTile.F = getH (adjacentTile.position, dest) + adjacentTile.G;
+				adjacentTile.id = maxID++;
+				adjacentTile.parent = 0;
+				open.Add (adjacentTile);
 			} else {
 				// should we add this spot to closed?
 			}
 		}
 
-		return finalPath;
+		int currentTileIndex = 0;
+
+		// now comes the actual algorithm
+
+		// pathfind until we've included the destination tile in our final path
+		while (findInList (dest, closed) == -1) {
+			// determine the next tile to inspect based on being the closest to destination
+			int nextTileIndex = findLowestScoreIndex (currentTileIndex, open);
+			currentTileIndex = nextTileIndex;
+
+			// retrieve this tile for inspection
+			currentTile = open [nextTileIndex];
+
+			// remove the tile from the open list
+			open.RemoveAt(nextTileIndex);
+
+			// add this tile to the closed list since it has been visited
+			closed.Add (currentTile);
+
+			// look in the four directions around this tile
+			for (int i = 0; i < 4; i++) {
+				Vector2 testPos = directions [i] + currentTile.position;
+
+				// we can't add tiles that exist as part of an obstacle or are already in the closed list
+				if (!gc.inObstacle (testPos) && findInList (testPos, closed) == -1) {
+					// now that we know this is a viable tile, check if it's already been added
+					int testIndex = findInList (testPos, open);
+
+					if (testIndex == -1) {					
+						// create a new gameTile for this location
+						adjacentTile.position = testPos;
+						adjacentTile.G = currentTile.G + 1;
+						adjacentTile.F = getH (testPos, dest) + adjacentTile.G;
+						adjacentTile.id = maxID++;
+						adjacentTile.parent = currentTile.id;
+						open.Add (adjacentTile);
+					} else {
+						// we've seen this tile before, so check to see if it's new F value is
+						// improved from the G based on the current position
+						if (currentTile.G + 1 < open [testIndex].G) {
+							adjacentTile = open [testIndex];
+							// subtract from the F the difference between the new G and the old G
+							adjacentTile.F -= (currentTile.G + 1 - adjacentTile.G);
+							// update the G
+							adjacentTile.G = currentTile.G + 1;
+
+							// update the parent too
+							adjacentTile.parent = currentTile.id;
+
+							open [testIndex] = adjacentTile;
+						}
+					}
+				}
+			}
+		}
+
+		return buildPath(dest,closed);
 	}
 
-	private int H(Vector2 loc) {
+	private int getH(Vector2 start, Vector2 dest) {
 		// this is as simple as finding the manhattan distance from this position to the destination
-		Vector2 diff = loc - B;
+		Vector2 diff = start - dest;
 		return (int)(Mathf.Abs (diff.x) + Mathf.Abs (diff.y));
 	}
 
-//	private int G(Vector2 loc) {
-//
-//	}
+	private int findLowestScoreIndex(int currentID, List<gameTile> tiles) {
+		int min = int.MaxValue;
+		int minIndex = 0;
+		// return the index of the tile with the lowest score
+		for (int i = 0; i < tiles.Count; i++) {
+			if (tiles[i].F < min) {
+				// save the new minimum
+				min = tiles [i].F;
+				minIndex = i;
+			} else if (tiles[i].F == min && tiles[i].parent == currentID) {
+				// break a tie by going with the tile adjacent to the most recently inspected tile
+				min = tiles [i].F;
+				minIndex = i;
+			}
+		}
 
-//	private int F(Vector2 loc) {
-//		return H (loc) + G (loc);
-//	}
+		return minIndex;
+	}
+
+	private int findInList(Vector2 loc, List<gameTile> tiles) {
+		// cycle through the list and see if any tiles are at this location
+		// return the index of the tile in the list if so, otherwise return -1
+		for (int i = 0; i < tiles.Count; i++) {
+			if (loc == tiles [i].position) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	private int findInList(int id, List<gameTile> tiles) {
+
+		// overloaded to work with id rather than location
+
+		// cycle through the list and see if any tiles are at this location
+		// return the index of the tile in the list if so, otherwise return -1
+		for (int i = 0; i < tiles.Count; i++) {
+			if (id == tiles [i].id) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	private List<Vector2> buildPath(Vector2 dest, List<gameTile> tiles) {
+		// walk backwards from the destination tile to the starting tile
+		// (don't add the starting tile because we know where we are)
+
+		List<Vector2> path = new List<Vector2> ();
+
+		int currentID = findInList (dest, tiles);
+
+		while (tiles [currentID].id != 0) {
+			// insert the position at the front of the path
+			path.Insert(0,tiles [currentID].position);
+			// update the current id
+			currentID = findInList(tiles [currentID].parent,tiles);
+		}
+
+		return path;
+	}
 }
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,20 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
-	private GameController gc;
-	private Vector2 dest;
+	public float moveSpeedPercent;	// the percent of the distance between tiles that the character will move in each update
+	private Vector3 updateMove;
+	private GameController gc;		// reference to the game controller
+	private Vector2 dest;			// the current destination of this character
+	private List<Vector2> path;		// the remaining steps in the path to reach the destination
+	private bool moving;			// check for whether the character is in motion or not
+	private AStar pathfinder;		// the controller for the A* pathfinding algorithm
+	private Vector2 nextTile;
+
+
 
 	// Use this for initialization
 	void Start () {
 		gc = (GameController)GameObject.Find ("GameController").GetComponent (typeof(GameController));
 		transform.position = gc.emptyPointInGame ();
+		dest = gc.getDestination ();
+		pathfinder = new AStar ();
+		moving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// check if x has moved
-		// if so, update destination position
-		// restart algorithm
+		if (dest != gc.getDestination ()) {
+			// if so, update destination position
+			reroute ();
+		}
+
+
+		if (moving) {
+			if ((Vector2)(transform.position) != nextTile) {
+				// still on the way to the next tile, so keep going
+				takeStep ();
+			} else {
+				moving = false;
+				// remove the most recent tile in the path
+				path.RemoveAt(0);
+			}
+		} else {
+			// we either:
+			// A) haven't started along a path or have reached the destination
+			// B) we have just made it to the next tile
+			// only move in B
+			if (path.Count > 0) {
+				// we are stationary and there are more movements to make
+				nextTile = path[0];
+				updateMove = moveSpeedPercent * ((Vector3)nextTile - transform.position);
+
+				moving = true;
+			}
+
+		}
 		
+	}
+
+	private void reroute() {
+		// figure out where we need to go
+		dest = gc.getDestination();
+		// build the path from the current position to there
+		path = pathfinder.navigate (transform.position, dest);
+	}
+
+
+	private void takeStep() {
+		// move along the path to the next tile
+		transform.position += updateMove;
 	}
 }
