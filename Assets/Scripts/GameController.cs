58b37771156400f69089b98353d603e15186cb00
cancub <alfred.kenny@mail.mcgill.ca>
2017-03-23 21:39:27 -0400

    adding game controller
    will be sued to spawn characters and reference the inObstacle function

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject obstacles; 
+	public GameObject playingField;
+	public GameObject borders;
+	private Vector2 bottomLeft;
+
+	// Use this for initialization
+	void Start () {
+		bottomLeft = new Vector2 (-1f * playingField.GetComponent<MeshFilter> ().mesh.bounds.size.x / 2f,
+			-1 * playingField.GetComponent<MeshFilter> ().mesh.bounds.size.z / 2f);
+		print (bottomLeft);
+		// create a character and place them in a location
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool inObstacle(float x, float y) {
+		foreach (Transform child in obstacles.transform) {
+			// get the rectangle that describes the top surface of the object
+			Vector2 pos = new Vector2 (child.position.x - child.localScale.x / 2f, 
+				child.position.y - child.localScale.y / 2f) - bottomLeft;
+			Vector2 size = new Vector2 (child.localScale.x, child.localScale.y);
+			Rect rect = new Rect(pos,size);
+			// this child rectangle contains the object
+			print(rect);
+
+			if (rect.Contains (new Vector2 (x, y) - bottomLeft)) {
+				return true;
+			}
+		}
+
+		foreach (Transform child in borders.transform) {
+			// get the rectangle that describes the top surface of the object
+			Vector2 pos = new Vector2 (child.position.x - child.localScale.x / 2f, 
+				child.position.y - child.localScale.y / 2f) - bottomLeft;
+			Vector2 size = new Vector2 (child.localScale.x, child.localScale.y);
+			Rect rect = new Rect(pos,size);
+			// this child rectangle contains the object
+			print(rect);
+
+			if (rect.Contains (new Vector2 (x, y) - bottomLeft)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
--- Assets/Scripts/MoveX.cs
+++ Assets/Scripts/MoveX.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveX : MonoBehaviour {
-	public GameObject obstacles; 
-	public GameObject playingField;
-	public GameObject borders;
-	private Vector2 bottomLeft;
+	private GameController gc;
 
 	void Start() {
 		// hide the object by placing it in the center
 		transform.position = new Vector3(2.5f,2.5f,0);
-		bottomLeft = new Vector2 (-1f * playingField.GetComponent<MeshFilter> ().mesh.bounds.size.x / 2f,
-			-1 * playingField.GetComponent<MeshFilter> ().mesh.bounds.size.z / 2f);
-		print (bottomLeft);
+		gc = (GameController)GameObject.Find ("GameController").GetComponent (typeof(GameController));
 	}
 
 	void Update() {
@@ -31,41 +26,11 @@ public class MoveX : MonoBehaviour {
 			float y = Mathf.Floor(hit.point.y) + 0.5f;
 
 			print (x.ToString() + "," + y.ToString());
-			if (!inObstacle(x,y)){
+			if (!gc.inObstacle(x,y)){
 				transform.position = new Vector3 (x, y, transform.position.z);
 			}
 		}
 	}
 
-	private bool inObstacle(float x, float y) {
-		foreach (Transform child in obstacles.transform) {
-			// get the rectangle that describes the top surface of the object
-			Vector2 pos = new Vector2 (child.position.x - child.localScale.x / 2f, 
-				child.position.y - child.localScale.y / 2f) - bottomLeft;
-			Vector2 size = new Vector2 (child.localScale.x, child.localScale.y);
-			Rect rect = new Rect(pos,size);
-			// this child rectangle contains the object
-			print(rect);
 
-			if (rect.Contains (new Vector2 (x, y) - bottomLeft)) {
-				return true;
-			}
-		}
-
-		foreach (Transform child in borders.transform) {
-			// get the rectangle that describes the top surface of the object
-			Vector2 pos = new Vector2 (child.position.x - child.localScale.x / 2f, 
-				child.position.y - child.localScale.y / 2f) - bottomLeft;
-			Vector2 size = new Vector2 (child.localScale.x, child.localScale.y);
-			Rect rect = new Rect(pos,size);
-			// this child rectangle contains the object
-			print(rect);
-
-			if (rect.Contains (new Vector2 (x, y) - bottomLeft)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
 }
