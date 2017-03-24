23cc3777ae5521a326a6d2378894f6cb0fcba070
cancub <alfred.kenny@mail.mcgill.ca>
2017-03-23 20:22:09 -0400

    first commit with the beginning of the game

---
+++ Assets/Scripts/MoveX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveX : MonoBehaviour {
+	public GameObject obstacles; 
+
+	void Start() {
+		// hide the object by placing it in the center
+		transform.position = new Vector3(2.5f,2.5f,0);
+	}
+
+	void Update() {
+		
+		if (Input.GetMouseButtonDown(0)) {
+			MoveTarget(Camera.main.ScreenPointToRay(Input.mousePosition));
+		}
+	}
+
+	private void MoveTarget(Ray ray) {
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			// get the x and y positions of the click
+			float x = Mathf.Floor(hit.point.x) + 0.5f;
+			float y = Mathf.Floor(hit.point.y) + 0.5f;
+
+			//				print (x.ToString() + "," + y.ToString());
+
+			transform.position = new Vector3 (x, y, transform.position.z);
+		}
+	}
+
+	private bool inObstacle(float x, float y) {
+		foreach (Transform child in obstacles.transform) {
+			// get the rectangle that describes the top surface of the object
+
+		}
+	}
+}
